50ef36e4ef5572cfb05089e79d770032fd30cda5
estefanochacinl <estefanochacinl@gmail.com>
2025-12-15 18:55:40 -0500

    se excluye dto de la cobertura

--- Shared/restatunt.Shared/DTOs/Order/ItemDto.cs
+++ Shared/restatunt.Shared/DTOs/Order/ItemDto.cs
@@ -1,8 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace restatunt.Shared.DTOs.Order;
 
+[ExcludeFromCodeCoverage]
 public class ItemDto
 {
     public required string Id { get; set; }
-    public required int Quantity { get; set; }
+    public required int Quantity { get; set; } 
 }
 
